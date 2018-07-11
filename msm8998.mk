3c9444b33ee165666d0b996f4ba351839b49b532
Monika Singh <monising@codeaurora.org>
2018-07-11 18:21:19 +0530

    MSM8998: Enable support for testVerifiedBootSupport in CtsSecurityTestCases
    CTS checks for android.software.verified_boot.xml in feature list so in
    order to CtsSecurityTestCases testVerifiedBootSupport to pass, it should
    be added to the feature list.
    
    Change-Id: I7250f34d4db66ff980854cde2fdb4dc863579ad4

--- msm8998.mk
+++ msm8998.mk
@@ -223,6 +223,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/qcom/msm8998/init.qti.qseecomd.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.qseecomd.sh
 
+# VB xml
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.verified_boot.xml:system/etc/permissions/android.software.verified_boot.xml
 # MSM IRQ Balancer configuration file
 PRODUCT_COPY_FILES += device/qcom/msm8998/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
